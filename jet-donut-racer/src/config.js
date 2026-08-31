5a8f36d0c463ce574648b65966b76a14a4e30bf5
Szentei András <111533089+Szenteia@users.noreply.github.com>
2026-08-31 13:34:08 +0200

    Add game configuration

---
+++ jet-donut-racer/src/config.js
@@ -0,0 +1,25 @@
+export const GAME_CONFIG = Object.freeze({
+  laps: 3,
+  trackWidth: 14,
+  trackSegments: 900,
+  minSpeed: 18,
+  cruiseSpeed: 30,
+  maxSpeed: 48,
+  acceleration: 18,
+  braking: 24,
+  drag: 7,
+  steeringSpeed: 10,
+  cameraDistance: 15,
+  cameraHeight: 7,
+  seed: 260831,
+});
+
+export const COLORS = Object.freeze({
+  sky: 0x080a12,
+  cyan: 0x20e3d2,
+  orange: 0xff6a2a,
+  violet: 0x7b4dff,
+  yellow: 0xffdd32,
+  white: 0xffffff,
+  black: 0x05060a,
+});
